3e34b10efe1345c633b58c269ba8660359b5cf19
Aung Myo Kyaw <amk.maeu@gmail.com>
2016-05-27 23:54:45 +0630

    style

--- js/index.js
+++ js/index.js
@@ -1,18 +1,14 @@
 jQuery(document).ready(function($) {
 	var errorMessage={message:"Unable to get data !  Please Try Again !",by:"Aung Myo Kyaw"};
-	var title='Random Quote Machine';	
-	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-	$("a.twitter-share-button").attr("data-text","fuck");
 	function getQuote(){
 		$.ajax({
 			url:'https://andruxnet-random-famous-quotes.p.mashape.com/',
 			type:'POST',
 			dataType:'json',
 			success:function(data){
-				$("#quote").html(data.quote);
+				$("#quote").html('"'+data.quote+'"');
 				$("#author").html(data.author);
 				$("a.twitter-share-button").attr("data-text",data.quote);
-				console.log(data);
 			},
 			error:function(error){
 				$("#quote").html(errorMessage.message);
